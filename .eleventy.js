0180952dc81379f2bbdfcf75e03eddd5381fd3ef
joshingmachine <josh@joshingmachine.com>
2021-01-29 14:41:09 -0500

    Make 404 page work

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy('src/keybase.txt')
 	eleventyConfig.addPassthroughCopy('src/robots.txt')
 	eleventyConfig.addPassthroughCopy('src/fonts')
+	eleventyConfig.addPassthroughCopy('src/images')
 
 	return {
 		dir: {

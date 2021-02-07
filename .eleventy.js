0cebf3d5c5ff177da95698e0686f072a7a9f0dd0
joshingmachine <josh@joshingmachine.com>
2021-02-06 20:36:20 -0500

    Add favicons

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy('src/keybase.txt')
 	eleventyConfig.addPassthroughCopy('src/robots.txt')
+	eleventyConfig.addPassthroughCopy({ 'src/favicons': '/' })
 	eleventyConfig.addPassthroughCopy('src/fonts')
 	eleventyConfig.addPassthroughCopy('src/images')
 
